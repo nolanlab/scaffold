d9f7c8c6caa5b2f9e7236c0fc1b16e24c0a36d2e
Pier Federico Gherardini <federico.gherardini@gmail.com>
2016-04-22 11:18:34 -0700

    Added parallelism to multi file clsutering

--- R/cluster_all_samples.R
+++ R/cluster_all_samples.R
@@ -176,7 +176,7 @@ cluster_fcs_files_in_dir <- function(wd, num.cores, col.names, num_clusters, num
 
 cluster_fcs_files_groups <- function(wd, files.list, num.cores, col.names, num_clusters, num_samples, asinh.cofactor)
 {
-    lapply(files.list,
+    parallel::mclapply(files.list, mc.cores = num.cores, mc.preschedule = FALSE,
                        process_files_groups, wd = wd, col.names = col.names, num_clusters = num_clusters, num_samples = num_samples, asinh.cofactor = asinh.cofactor)
     return(files.list)
 }
