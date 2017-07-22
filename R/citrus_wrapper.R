fa6d06707289a75943401344c5b90a434accdf1a
pfgherardini <federico.gherardini@gmail.com>
2017-07-21 17:23:17 -0700

    much better implementation of calculate_features

--- R/citrus_wrapper.R
+++ R/citrus_wrapper.R
@@ -1,8 +1,11 @@
 run_citrus_analysis <- function(citrus.features, endpoint, working.directory, model.type) {
     family <- NULL
     
-    if(is.character(endpoint))
+    if(is.character(endpoint) || is.factor(endpoint)) {
         family <- "classification"
+        endpoint <- as.factor(endpoint)
+        
+    }
     else
         family <- "continuoous"
 
@@ -35,6 +38,7 @@ calculate_abundance_features <- function(input.tab) {
     row.names(m) <- sapply(s, "[", 2)
     v <- unique(sapply(s, "[", 1))
     colnames(m) <- paste("cluster", 1:ncol(m), v)
+    
     m[is.na(m)] <- 0
     m <- m / rowSums(m)
     
@@ -42,6 +46,53 @@ calculate_abundance_features <- function(input.tab) {
     
     return(ret)
     
+    
+}
+
+# The matrix has to contain a single feature for multiple samples
+transpose_feature_matrix <- function(m) {
+    s <- strsplit(colnames(m), "@")
+    v <- unique(sapply(s, "[", 1))
+    stopifnot(length(v) == 1)
+    
+    m <- t(m)
+    colnames(m) <- paste("cluster", 1:ncol(m), v)
+    row.names(m) <- sapply(s, "[", 2)
+    
+    return(m)
+}
+
+calculate_features <- function(input.tab, marker.names, baseline = NULL) {
+    col.names <- sapply(marker.names, paste, "@", sep = "")
+    col.names <- paste(col.names, collapse = "|")
+    col.names <- grep(col.names, names(input.tab), value = T)
+    
+    m <- as.matrix(input.tab[, col.names])
+    
+    
+    ret <- lapply(marker.names, function(s) {
+        temp <- m[, grep(s, colnames(m))]
+        temp[is.na(temp)] <- 0
+        
+        if(s == "popsize") {
+            temp <- t(temp)
+            temp <- temp / rowSums(temp)
+            temp <- t(temp)
+        }
+        
+        if(!is.null(baseline)) {
+            bl <- paste(s, baseline, sep = "@")
+            temp <- temp / temp[, bl]
+        }
+        
+        temp <- transpose_feature_matrix(temp)
+        temp[!is.finite(temp)] <- 0
+        
+        return(temp)
+        
+    })
+    ret <- do.call(cbind, ret)
+    return(ret)
     
 }
 
